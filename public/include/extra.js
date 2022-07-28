efabb680900555a964905718180ac4e09c5f6b15
Rudolf Achter <rudolf.achter@gmail.com>
2022-07-29 00:06:09 +0200

    publish

---
+++ public/include/extra.js
@@ -0,0 +1,28 @@
+function flip_marketbox(event){
+  //alert("You open the mysterious box")
+  console.log(event)
+  console.log($(event.target).parent())
+  $("#market-messages").html(("You open " + $(event.target).parent().attr("name")))
+  $(event.target).parent().css('transform', 'rotateY(180deg)')
+
+  $(event.target).parent().parent().bind("click",get_marketbox_offer)
+  
+}
+
+function get_marketbox_offer(event){
+  //alert("You open the mysterious box")
+  console.log(event)
+  console.log($(event.target).parent())
+  $("#market-messages").html(("You investigate offer " + $(event.target).attr("alt")))
+  //$(event.target).parent().css('transform', 'rotateY(-180deg)')
+  
+}
+
+
+$( window ).on("load",function() {
+    console.log( "jquery ready!" );
+    //falls ich jquery brauche
+    $(".market-box").bind("click",flip_marketbox)
+    $(".market-box").bind("tap",flip_marketbox)
+    
+});
